07bf5fa0c9f5e222b5f1a671ac7e5bcc2ebde8c7
Ralf Northman <ralf.northman@spectrogon.com>
2018-11-22 09:30:11 +0100

    File type flags implemented.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use nom::{le_u32, le_f64};
 
 #[derive(Debug, PartialEq)]
 pub struct Spc {
+    file_type_flags: FileTypeFlags,
     file_version: FileVersion,
     regular_floats: bool,
     number_of_points: u32,
@@ -60,9 +61,43 @@ named!(
 );
 
 named!(
+    bit_to_bool((&[u8], usize)) -> bool,
+        alt!(
+            tag_bits!(u8, 1, 1) => { |_| true } |
+            tag_bits!(u8, 1, 0) => { |_| false }
+            )
+    );
+
+named!(
+    file_type_flags<FileTypeFlags>,
+    bits!(
+    do_parse!(
+        y16bit_precision: bit_to_bool >>
+        experiment_extension: bit_to_bool >>
+        multifile: bit_to_bool >>
+        z_randomly_ordered: bit_to_bool >>
+        z_not_even: bit_to_bool >>
+        custom_axis_labels: bit_to_bool >>
+        each_subfile_own_x_array: bit_to_bool >>
+        xy_file: bit_to_bool >>
+        (    FileTypeFlags {
+                 y16bit_precision,
+                 experiment_extension,
+                 multifile,
+                 z_randomly_ordered,
+                 z_not_even,
+                 custom_axis_labels,
+                 each_subfile_own_x_array,
+                 xy_file 
+            })
+        ))
+    );
+
+
+named!(
     pub main_tryout<Spc>,
     do_parse!(
-        take!(1) >>
+        file_type_flags: file_type_flags >>
         file_version: file_version >>
         take!(1) >>
         regular_floats: regular_floats >>
@@ -71,6 +106,7 @@ named!(
         last_x: le_f64 >>
         number_of_subfiles: le_u32 >>
         ( Spc{
+            file_type_flags,
             file_version,
             regular_floats,
             number_of_points,
